accee3514c44ddb22aae8944f98f88ec0035a8ea
Bullseye198 <martin.vojko@gmail.com>
2020-06-02 17:53:29 +0200

    Movie Detail Data Binding and Design Done

--- android-ui/src/main/java/com/example/movieu/movie/moviedetail/MovieDetailFragment.kt
+++ android-ui/src/main/java/com/example/movieu/movie/moviedetail/MovieDetailFragment.kt
@@ -1,17 +1,15 @@
 package com.example.movieu.movie.moviedetail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import coil.api.load
-import com.example.movieu.R
+import com.example.movieu.databinding.FragmentMovieDetailBinding
 import com.example.movieu.dependencyInjection.ViewModelFactory
 import com.example.movieu.movie.MovieDetailViewModel
-import dagger.android.DaggerApplication
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_movie_detail.*
 import javax.inject.Inject
@@ -19,6 +17,8 @@ import javax.inject.Inject
 class MovieDetailFragment : DaggerFragment() {
 
     private lateinit var viewModel: MovieDetailViewModel
+
+    private lateinit var binding: FragmentMovieDetailBinding
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -34,10 +34,10 @@ class MovieDetailFragment : DaggerFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MovieDetailViewModel::class.java)
         viewModel.handleEvent(MovieDetailEvent.OnStart(imdbID))
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        return binding.root
     }
 
     override fun onStart() {
@@ -51,6 +51,14 @@ class MovieDetailFragment : DaggerFragment() {
             viewLifecycleOwner,
             Observer { movie ->
                 movieDetailView.load(movie.poster.replace("http:", "https:"))
+                binding.lblMovieTitle.text = movie.title
+                binding.lblMovieYear.text = movie.year
+                binding.lblMovieRuntime.text = movie.runtime
+                binding.lblMoviePlot.text = movie.plot
+                binding.lblMovieGenre.text = movie.genre
+                binding.lblMovieLanguage.text = movie.language
+                binding.lblMovieCast.text = movie.actors
+                binding.lblMovieDirector.text = movie.director
             }
         )
     }
