fa29d4b6af073cbe299bb2eca47131520b4708b5
Bullseye198 <martin.vojko@gmail.com>
2020-06-10 14:43:47 +0200

    ObserveMovieDetailUseCase Abstracted

--- android-ui/src/main/java/com/example/movieu/movie/moviedetail/MovieDetailViewModel.kt
+++ android-ui/src/main/java/com/example/movieu/movie/moviedetail/MovieDetailViewModel.kt
@@ -32,8 +32,7 @@ class MovieDetailViewModel @Inject constructor(
     }
 
     private fun observeMovieDetail(imdbID: String) {
-        observeMovieDetailUseCase.requestMovieDetail(
-            imdbID,
+        observeMovieDetailUseCase.invokeUseCase(
             object : DisposableSubscriber<MovieDetail>() {
                 override fun onComplete() {
 
@@ -46,7 +45,7 @@ class MovieDetailViewModel @Inject constructor(
                 override fun onError(t: Throwable?) {
                     throw Exception("Subscription failed at ${t?.localizedMessage}")
                 }
-            })
+            }, ObserveMovieDetailUseCase.Params(imdbID))
     }
 
     fun refresh(imdbID: String) {
