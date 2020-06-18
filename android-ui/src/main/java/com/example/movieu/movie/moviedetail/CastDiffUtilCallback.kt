fcd647d7a4d78b77d07e42f10a41738675a1daf2
Bullseye198 <martin.vojko@gmail.com>
2020-06-18 18:13:58 +0200

    Cast Adapter Works

---
+++ android-ui/src/main/java/com/example/movieu/movie/moviedetail/CastDiffUtilCallback.kt
@@ -0,0 +1,14 @@
+package com.example.movieu.movie.moviedetail
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.domain.tmdbmovie.model.Cast
+
+class CastDiffUtilCallback: DiffUtil.ItemCallback<Cast>() {
+    override fun areItemsTheSame(oldItem: Cast, newItem: Cast): Boolean {
+        return oldItem.name == newItem.name
+    }
+
+    override fun areContentsTheSame(oldItem: Cast, newItem: Cast): Boolean {
+        return oldItem == newItem
+    }
+}
--- android-ui/src/main/java/com/example/movieu/movie/moviedetail/MovieDetailState.kt
+++ android-ui/src/main/java/com/example/movieu/movie/moviedetail/MovieDetailState.kt
@@ -1,11 +1,11 @@
 package com.example.movieu.movie.moviedetail
 
 import com.example.domain.movie.model.OMDbBaseInformation
+import com.example.domain.tmdbmovie.model.Cast
 import com.example.domain.tmdbmovie.model.Credits
 import com.example.domain.tmdbmovie.model.TMDbMovieDetail
 
 data class MovieDetailState(
     val OMDbBaseInformation: OMDbBaseInformation? = null,
-    val tmDbMovieDetail: TMDbMovieDetail? = null,
-    val tmdbCredits: Credits? = null
+    val tmDbMovieDetail: TMDbMovieDetail? = null
 )
